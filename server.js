d07f588859e4d5de38b78b4eb80db072524eb9e4
khalid <daliardodali@gmail.com>
2025-03-14 12:30:42 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
